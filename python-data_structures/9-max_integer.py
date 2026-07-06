6ee3b6b64450b8fae44d9774220bca239dab6766
bkayihura <b.kayihura@alustudent.com>
2026-07-06 05:54:11 -0700

    add python-data_structures tasks 0-12

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,8 +2,8 @@
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return None
-    biggest = my_list[0]
-    for item in my_list:
-        if item > biggest:
-            biggest = item
-    return biggest
+    m = my_list[0]
+    for i in my_list:
+        if i > m:
+            m = i
+    return m
