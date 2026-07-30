64283c9aa867bc41a88b675c7e35040bbf6fb40f
bkayihura <bkayihura@alustudent.com>
2026-07-30 12:36:35 -0700

    Simplify 0-privileges.sql to match canonical two SHOW GRANTS statements

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
 -- Lists all privileges of user_0d_1 and user_0d_2
-SELECT * FROM information_schema.user_privileges WHERE grantee LIKE "'user_0d_1'%";
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
