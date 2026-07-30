edbf5be1f401f8bb187a5eabf8b1eb2f163b84a9
bkayihura <bkayihura@alustudent.com>
2026-07-30 12:24:31 -0700

    Restore information_schema privileges query in 0-privileges.sql

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all privileges of user_0d_1 and user_0d_2
+SELECT * FROM information_schema.user_privileges WHERE grantee LIKE "'user_0d_1'%";
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
